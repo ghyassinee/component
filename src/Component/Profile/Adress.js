1a12448bfaf2e10cfc15c17f0499a98498c64308
ghyassinee <yassine.ghoul@esprit.tn>
2021-08-17 11:39:31 +0200

    a

---
+++ src/Component/Profile/Adress.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Adress extends Component {
+    state = {}
+    render() {
+        return (
+         
+            <div  style={{color: "white"}}>
+         <span>Aouina tunis tunisia</span>
+            </div>
+        );
+    }
+}
+
+export default Adress;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class FullName extends Component {
+    state = {}
+    render() {
+        return (
+         
+            <div style={{color: "white",maxHeight:'40px'}}>
+          
+          <h1 >Ghoul Yassine</h1>
+            </div>
+        );
+    }
+}
+
+export default FullName;
